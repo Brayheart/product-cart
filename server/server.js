87807b0dc0b642ebd3c56999d78a62400a83e5ca
Andy Huynh <huynha859@gmail.com>
2022-06-15 17:21:36 -0700

    setup initial routes for all products

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
+const routes = require('./routes');
+
 app.use(express.json());
 
+app.use('/api', routes);
 
 app.get('/', (req, res) => res.json('Hello Rugs USA Team!'))
 
