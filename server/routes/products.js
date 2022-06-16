fb08977bdc09db11d715b7458a34b2dbc7f4cd28
Andy Huynh <huynha859@gmail.com>
2022-06-15 17:38:20 -0700

    change products json file add products.json to gitignore

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const products = require('../data/product-list.json');
+const products = require('../data/products.json');
 
 router.get('/all', (req, res) => res.json(products));
 
