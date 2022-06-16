207b51bad7153c2ddaf258ba12a063d7bb0ff776
Andy Huynh <huynha859@gmail.com>
2022-06-15 18:41:53 -0700

    set up react to bundle from react through babel

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = () => {
+    return <h1>Hello Rugs USA!</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
-  output: {
-    path: path.resolve(__dirname, './public'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-      static: path.resolve(__dirname, './public'),
-      port: 3001
-  },
+    entry: path.resolve(__dirname, './src/index.js'),
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: path.resolve(__dirname, './public'),
+        port: 3001
+    },
 };
