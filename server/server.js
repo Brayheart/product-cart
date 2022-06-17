d9cb384ea1b46fa54eebfc6fa297bcf680b4ba45
Andy Huynh <huynha859@gmail.com>
2022-06-17 01:56:24 -0700

    add build to server start script, server static files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-var cors = require('cors')
+const path = require('path');
+const cors = require('cors')
 
 const port = process.env.PORT || 8080;
 
@@ -7,13 +8,15 @@ const app = express();
 
 const routes = require('./routes');
 
+app.use(cors())
+
 app.use(express.json());
 
-app.use(cors())
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.use('/api', routes);
 
-app.get('/', (req, res) => res.json('Hello Rugs USA Team!'))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')))
 
 app.listen(port, () => {
     console.log(`Server app started on port ${port}`);
