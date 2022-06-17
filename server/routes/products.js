789dfe095d392cd046f6057440778fc554c3160d
Andy Huynh <huynha859@gmail.com>
2022-06-17 11:05:30 -0700

    update scripts and add a root package for deployment

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const products = require('../data/products.json');
+const products = require('../products.json');
 
 router.get('/all', (req, res) => res.json(products));
 
